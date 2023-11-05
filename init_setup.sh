ffad933fbc06fe872d7d41d28965d72daa8bedb1
nikhil-xyz <sonkusarenikhil@sggs.ac.in>
2023-11-05 21:05:49 +0530

    logger file updated

--- init_setup.sh
+++ init_setup.sh
@@ -1,12 +1,18 @@
 echo [$(date)]: "START"
 
-echo [$(date)]: "creating env with python 3.11 version"
-conda create --prefix ./env python=3.11 -y
 
-echo [$(date)]: "activating the environment"
+echo [$(date)]: "creating env with python 3.8 version" 
+
+
+conda create --prefix ./env python=3.8 -y
+
+
+echo [$(date)]: "activating the environment" 
+
 source activate ./env
 
-echo [$(date)]: "installing the dev requirements"
+echo [$(date)]: "installing the dev requirements" 
+
 pip install -r requirements.txt
 
-echo [$(date)]: "END"
+echo [$(date)]: "END" 
