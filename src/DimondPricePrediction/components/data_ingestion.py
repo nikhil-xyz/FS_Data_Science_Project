50b77080813414b37f306c6be77b30f014633188
nikhil-xyz <sonkusarenikhil@sggs.ac.in>
2023-11-13 21:38:34 +0530

    updated structure

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -1,8 +1,25 @@
+import numpy as np
+import pandas as pd
+import os
+from src.DimondPricePrediction.logger import logging
 
+from sklearn.model_selection import train_test_split
+from src.DimondPricePrediction.exception import customexception
+from dataclasses import dataclass
+from pathlib import Path
 
 class DataIngestion:
     def __init__(self):
         pass
 
     def initiate_data_ingestion(self):
-        pass
+        logging.info('Data Injestion Started')
+
+        try:
+            data = pd.read_csv(os.path.join('notebook/data', 'gemstone.csv'))
+            logging.info('I have read the dataset as a dataframe')
+
+            train_data, test_data = train_test_split(data, test_size=0.25)
+            logging.info('train_test_split completed')
+        except Exception as e:
+            pass
